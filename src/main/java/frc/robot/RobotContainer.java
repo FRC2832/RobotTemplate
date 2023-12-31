3a9d4a9294413ee03cddbc5fa6a5a82b68787585
Steven <scarmain@gmail.com>
2023-12-31 12:27:56 -0500

    Add 23 swerve hardware

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,7 @@ import org.livoniawarriors.swerve.DriveXbox;
 import org.livoniawarriors.swerve.MoveWheels;
 import org.livoniawarriors.swerve.SwerveDriveSim;
 import org.livoniawarriors.swerve.SwerveDriveTrain;
+import org.livoniawarriors.swerve.SwerveHw23;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -58,6 +59,7 @@ public class RobotContainer {
             odometry.setGyroHardware(new SimSwerveGyro(swerveDrive));
         } else {
             //competition robot
+            swerveDrive = new SwerveDriveTrain(new SwerveHw23(), odometry);
             odometry.setGyroHardware(new Pigeon2Gyro(0));
         }
         
